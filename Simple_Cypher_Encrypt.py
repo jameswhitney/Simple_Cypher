f0e7faf5a709232d068c78d8310c133dc4665d46
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2019-05-22 09:06:12 -0400

    Initial Commit for encryption file

---
+++ Simple_Cypher_Encrypt.py
@@ -0,0 +1,15 @@
+################################
+# Encrypt a plain text message #
+# using a simple Ceaser Cypher #
+################################
+
+# import string in order to access builtin string methods
+import string
+
+
+def encrypt(message, shift_value):
+
+    '''
+    INPUT: message as string and an int for shift_value
+    OUTPUT: Encrypted text based on shift_value for Ceaser Cypher
+    '''
