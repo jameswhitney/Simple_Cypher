eceee2a28a0e60ee5d1d3144690dc4d1d64c9085
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2019-05-22 16:05:20 -0400

    refactor: Moved all functions into this file

--- Simple_Cypher_Encrypt.py
+++ Simple_Cypher_Encrypt.py
@@ -51,3 +51,74 @@ def encrypt(message, shift_value):
             encrypted_message[i] = char
 
     return ''.join(encrypted_message)
+
+
+############################################
+# This file decrypts the encrypted message #
+# from the Simple_Cypher_Encrypt file      #
+############################################
+
+
+def decrypt(message, shift_value):
+    '''
+    INPUT: Encrypted message from Ceaser Cypher and the integer shift value
+    OUTPUT: The decrypted original message
+    '''
+
+    # Cast message string to a list and save to a variable
+    # for later use
+    decrypted_message = list(range(len(message)))
+
+    # Create a list of each individual letter alphabet in original order
+    alphabet = string.ascii_lowercase
+
+    # Slice the alphabet based on the shift_value. The first half
+    # should be sliced up to the shift_value. The second half
+    # should be sliced starting at the shift_value till the end of the string.
+    first_half = alphabet[:shift_value]
+    second_half = alphabet[shift_value:]
+
+    # Create shifted alphabet by concatenating the second_half of the alphabet
+    # with the first_half of the alphabet
+    shifted_alphabet = second_half + first_half
+
+    # Iterate over the message string to get the index and characters
+    # within the message string
+    for i, char in enumerate(message.lower()):
+
+        # If the character is part of the alphabet
+        # add it to the decrypted phrase array.
+        # If character is not part of the alphabet, add it
+        # to the decrypted message.
+        if char in alphabet:
+
+            shifted_index = shifted_alphabet.index(char)
+            decrypted_message[i] = alphabet[shifted_index]
+
+        else:
+
+            decrypted_message[i] = char
+
+    return ''.join(decrypted_message)
+
+
+##################################
+# Use a brute force method to    #
+# decrypt an encrypted message   #
+# if encryption key is not known #
+##################################
+
+
+def brute_force_decryption(message):
+    '''
+    INPUT: A Caeser Cypher encrypted message
+    OUTPUT: Prints all possible Ceasar Cypher messages
+            Only one output should be the decrypted phrase
+    '''
+    
+    alphabet = list(range(len(string.ascii_lowercase)))
+    
+    for n in alphabet:
+        print('Using shift value: {}'.format(n))
+        print(decrypt(message, n))
+        print('\n')
