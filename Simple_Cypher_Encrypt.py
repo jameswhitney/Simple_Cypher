07636541c70ebdaf186f70a12b160fbf00ea951f
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2019-05-22 09:15:13 -0400

    feat: Creates shifted alphabet for Ceaser Cypher

--- Simple_Cypher_Encrypt.py
+++ Simple_Cypher_Encrypt.py
@@ -13,3 +13,17 @@ def encrypt(message, shift_value):
     INPUT: message as string and an int for shift_value
     OUTPUT: Encrypted text based on shift_value for Ceaser Cypher
     '''
+
+    # Convert message string to a list and save to encrypted_message variable
+    encrypted_message = list(range(len(message)))
+
+    # Create a lower case alphabet
+    alphabet = string.ascii_lowercase
+
+    # Create a shifted version of the alphabet based on the shift_value
+    # The shift_value determines where to split the alphabet based on index value
+    first_half = alphabet[:shift_value]
+    second_half = alphabet[shift_value:]
+
+    shifted_alphabet = second_half + first_half
+
