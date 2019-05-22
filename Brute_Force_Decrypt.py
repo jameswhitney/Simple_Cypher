c5e0ccaf9ba3c5f9edf917b61383c9dc9b5b2dd6
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2019-05-22 10:44:18 -0400

    feat: Decrypts message using brute force method

--- Brute_Force_Decrypt.py
+++ Brute_Force_Decrypt.py
@@ -4,6 +4,9 @@
 # decrypt an encrypted message #
 ################################
 
+import string
+from Simple_Cypher_Decrypt import decrypt
+
 def brute_force_decryption(message):
     '''
     INPUT: A Caeser Cypher encrypted message
@@ -11,3 +14,9 @@ def brute_force_decryption(message):
             Only one output should be the decrypted phrase
     '''
     
+    alphabet = list(range(len(string.ascii_lowercase)))
+    
+    for n in alphabet:
+        print('Using shift value: {}'.format(n))
+        print(decrypt(message, n))
+        print('\n')
