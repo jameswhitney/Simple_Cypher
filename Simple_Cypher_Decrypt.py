e64091cf00a6f3304ddb7a4a9c8f5aff14f8c781
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2019-05-22 09:55:39 -0400

    Initial Commit

---
+++ Simple_Cypher_Decrypt.py
@@ -0,0 +1,13 @@
+
+############################################
+# This file decrypts the encrypted message #
+# from the Simple_Cypher_Encrypt file      #
+############################################
+
+import string
+
+def decrypt(message, shift_value):
+    '''
+    INPUT: Encrypted message from Ceaser Cypher and the integer shift value
+    OUTPUT: The decrypted original message
+    '''
