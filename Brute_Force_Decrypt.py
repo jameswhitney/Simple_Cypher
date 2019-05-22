17d06f8f5b56ec7bf8a0498d77ac97a77f9f2401
James Whitney <33575806+jameswhitney@users.noreply.github.com>
2019-05-22 10:21:42 -0400

    Initial Commit

---
+++ Brute_Force_Decrypt.py
@@ -0,0 +1,13 @@
+
+################################
+# Use a brute force method to  #
+# decrypt an encrypted message #
+################################
+
+def brute_force_decryption(message):
+    '''
+    INPUT: A Caeser Cypher encrypted message
+    OUTPUT: Prints all possible Ceasar Cypher messages
+            Only one output should be the decrypted phrase
+    '''
+    
